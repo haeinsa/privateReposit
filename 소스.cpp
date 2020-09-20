29bd1a6c038d74e56e3be09e9dd83f9283e8a9b1
haeinsa <wo4014@gmail.com>
2020-09-20 21:41:12 +0900

    no message

--- 소스.cpp
+++ 소스.cpp
@@ -6,16 +6,16 @@ int capacity;
 int lastIdx;
 
 void Init(int size) {
-	if(arr !=NULL)
-	   free(arr);
+	if (arr != NULL)
+		free(arr);
 	capacity = size;
 	lastIdx = 0;
-	arr = (int*)malloc(sizeof(int)*capacity);
+	arr = (int*)malloc(sizeof(int) * capacity);
 }
 
 void ReallocateArray();
 void Insert(int val) {
-	if (lastIdx+1 >= capacity) 
+	if (lastIdx + 1 >= capacity)
 		ReallocateArray();
 	arr[lastIdx++] = val;
 }
@@ -23,15 +23,16 @@ void Insert(int val) {
 void ReallocateArray() {
 	size_t duumy = (size_t)realloc(arr, sizeof(int) * capacity * 2);
 	capacity *= 2;
-	
+
 }
 
 void InsertAt(int idx, int val) {
 	if (lastIdx + 1 >= capacity)
 		ReallocateArray();
-	for (int i = lastIdx + 1; i > idx; i--) 
+	for (int i = lastIdx + 1; i > idx; i--)
 		arr[i] = arr[i - 1];
 	arr[idx] = val;
+	lastIdx++;
 }
 
 void pushBack(int val) {
@@ -48,7 +49,25 @@ int RemoveAt(int idx) {
 	int temp = arr[idx];
 	for (int i = idx; i < lastIdx - 1; i++)
 		arr[i] = arr[i + 1];
+	lastIdx--;
 	return temp;
+}
+
+void Reaplce(int idx, int val) {
+	if (idx >= lastIdx) {
+		printf("you are trying to replace with invalid index \n");
+		return;
+	}
+
+	arr[idx] = val;
+}
+
+int At(int idx) {
+	if (idx >= lastIdx) {
+		printf("you are trying to access with invalid index \n");
+		return NULL;
+	}
+	return arr[idx];
 }
 
 void clear() {
@@ -56,22 +75,53 @@ void clear() {
 		arr[i] = 0;
 }
 
+void quickSortProcedure(int left, int right);
 void quickSort() {
 	quickSortProcedure(0, lastIdx - 1);
 }
 
 void quickSortProcedure(int left, int right) {
-	int pivotValue = arr[left];
-	int j;
-	for (int i,j = left + 1; i <= right; i++) {
-		if(pivo)
+	if (left >= right)
+		return;
+	int pivotIdx = (left + right) / 2;
+	int pivotVal = arr[pivotIdx];
+
+	int i = left;
+	int j = right;
+	while (i < j) {
+		while (arr[i] <= pivotVal)
+			i++;
+		while (arr[j] >= pivotVal)
+			j++;
+
+		if (i <= j) {
+			int temp = arr[j];
+			arr[j] = pivotVal;
+			arr[pivotIdx] = temp;
+
+			quickSortProcedure(left, pivotIdx - 1);
+			quickSortProcedure(pivotIdx + 1, right);
+			break;
+		}
+		else {
+			int temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+		}
 	}
 }
 
 int main() {
-	Init(1);
-	Insert(5);
-	Insert(15);
-	printf("%d \n", arr[0]);
+	Init(15);
+	Insert(10);
+	Insert(12);
+	Insert(166);
+	Insert(1);
+	for (int i = 0; i < lastIdx; i++)
+		printf("%d ,", At(i));
+	quickSort();
+	printf("\n");
+	for (int i = 0; i < lastIdx; i++)
+		printf("%d ,", At(i));
 	return 0;
 }
